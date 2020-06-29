c87f6c94b7a01c5d8992652fec2f54575e2933a6
Isaac Sunoo <isaacsunoo@gmail.com>
2020-06-29 10:18:28 -0600

    refactored theme form

--- src/components/ThemeForm/ThemeForm.js
+++ src/components/ThemeForm/ThemeForm.js
@@ -4,20 +4,18 @@ import { Form } from 'rff-wrapper';
 import arrayMutators from 'final-form-arrays';
 import RenderForm from './components/RenderForm/RenderForm';
 
-export const ThemeForm = ({ onSubmit, theme }) => {
-  return (
-    <Form
-      onSubmit={onSubmit}
-      initialValues={{
-        storyBlokAccessToken: process.env.REACT_APP_STORYBLOK_ACCESS_TOKEN,
-        theme,
-      }}
-      mutators={{ ...arrayMutators }}
-    >
-      <RenderForm />
-    </Form>
-  );
-};
+export const ThemeForm = ({ onSubmit, theme }) => (
+  <Form
+    onSubmit={onSubmit}
+    initialValues={{
+      storyBlokAccessToken: process.env.REACT_APP_STORYBLOK_ACCESS_TOKEN,
+      theme,
+    }}
+    mutators={{ ...arrayMutators }}
+  >
+    <RenderForm />
+  </Form>
+);
 
 export default ThemeForm;
 
