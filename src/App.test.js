d3a452044e83de1a9e79c7ae52f0ef3d8f713138
alex james <apjames93@gmail.com>
2020-07-06 13:24:11 -0600

    cc fix

--- src/App.test.js
+++ src/App.test.js
@@ -3,21 +3,14 @@ import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
 import App from './App';
 
-function setup() {
-  const props = {};
-  const comp = shallow(<App {...props} />);
-  return { comp, props };
-}
-
 describe('<App />', () => {
   it('renders App', () => {
-    const { comp } = setup();
+    const comp = shallow(<App />);
     expect(comp).toBeDefined();
   });
 
   test('snapshot', () => {
-    const { props } = setup();
-    const tree = renderer.create((<App {...props} />));
+    const tree = renderer.create((<App />));
     expect(tree).toMatchSnapshot();
   });
 });
